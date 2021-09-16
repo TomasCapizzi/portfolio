884fc86832a5fad7cbdcca0fcb7b5c7f69b4c6eb
Tomas Capizzi <tomas.capizzi@hotmail.com>
2021-09-16 00:11:25 -0300

    Resposive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import {HiMenuAlt3} from 'react-icons/hi'
+import {FaHome} from 'react-icons/fa'
 
 export default function Navbar(){
 
+    function dropMenu(){        
+        const menuHamb = document.getElementById('menu-hamb')
+        menuHamb.classList.toggle('on')
+    }
+
     return (
+        <>
         <nav className='menu'>
             <ul>
-                <a href='/' rel="noreferrer"><li>Home</li></a>
+                <a href='/' rel="noreferrer"><li><FaHome/></li></a>
                 <a href="#about-me" rel="noreferrer"><li>About Me</li></a>
                 <a href="#projects" rel="noreferrer"><li>Projects</li></a>
                 <a href="#contact" rel="noreferrer"><li>Contact</li></a>
@@ -16,6 +24,22 @@ export default function Navbar(){
                 <li>ES</li>
             </ul>
         </nav>
+        <nav className='menu-hamb'>
+            <a href='/' rel="noreferrer"><FaHome/></a>
+            <button onClick={dropMenu}><HiMenuAlt3/></button>
+            <div className='hamb-links' id='menu-hamb'>
+                <ul>
+                    <a href="#about-me" rel="noreferrer"><li>About Me</li></a>
+                    <a href="#projects" rel="noreferrer"><li>Projects</li></a>
+                    <a href="#contact" rel="noreferrer"><li>Contact</li></a>
+                </ul>
+                <div>
+                    <button>EN</button>
+                    <button>ES</button>
+                </div>
+            </div>
+        </nav>
+        </>
     ) 
    
 }
